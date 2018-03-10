f19ebc9686c3096920f50b348737cf61d1812e74
Wali Rahman <wali.rahman89@yahoo.com>
2018-03-10 21:14:36 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,6 +1,24 @@
 CREATE TABLE books (
   id INTEGER PRIMARY KEY,
-  author_id INTEGER,
-  genre_id INTEGER,
+  title TEXT,
+  year INTEGER,
+  series_id INTEGER
+);
 
-)
+CREATE TABLE series (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  author_id INTEGER,
+  subgenre_id INTEGER
+);
+
+CREATE TABLE subgenres (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+
+CREATE TABLE authors (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
